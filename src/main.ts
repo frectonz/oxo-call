5bc5dbb7503f529dc0781817d0c8a6bed84b7ac2
frectonz <fraol0912@gmail.com>
2025-02-27 19:52:16 +0300

    feat: add a parse move function

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import "./style.css";
-
 import { Elm } from "./Main.elm";
-import confetti from "canvas-confetti";
+import { launchConfetti } from "./confetti";
 
 const app = Elm.Main.init({
   node: document.getElementById("app")!,
@@ -10,40 +9,3 @@ const app = Elm.Main.init({
 app.ports.launchConfetti.subscribe(() => {
   launchConfetti();
 });
-
-function randomInRange(min: number, max: number) {
-  return Math.random() * (max - min) + min;
-}
-
-function launchConfetti() {
-  const duration = 10 * 1000;
-  const animationEnd = Date.now() + duration;
-  const defaults = {
-    startVelocity: 30,
-    spread: 360,
-    ticks: 60,
-    zIndex: 0,
-  };
-
-  const interval = setInterval(function () {
-    const timeLeft = animationEnd - Date.now();
-
-    if (timeLeft <= 0) {
-      return clearInterval(interval);
-    }
-
-    var particleCount = 100 * (timeLeft / duration);
-
-    confetti({
-      ...defaults,
-      particleCount,
-      origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 },
-    });
-
-    confetti({
-      ...defaults,
-      particleCount,
-      origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 },
-    });
-  }, 250);
-}
---
+++ src/parseMove.ts
@@ -0,0 +1,58 @@
+import { ArkErrors, type } from "arktype";
+import { GoogleGenerativeAI, SchemaType } from "@google/generative-ai";
+
+const genAI = new GoogleGenerativeAI(import.meta.env.VITE_GEMINI_KEY);
+const model = genAI.getGenerativeModel({
+  model: "gemini-1.5-pro",
+  generationConfig: {
+    responseMimeType: "application/json",
+    responseSchema: {
+      description: "Tic Tac Toe Move Parser",
+      type: SchemaType.STRING,
+      enum: [
+        "TOP_LEFT",
+        "TOP_MIDDLE",
+        "TOP_RIGHT",
+        "MODDLE_LEFT",
+        "MIDDLE_MIDDLE",
+        "MIDDLE_RIGHT",
+        "BOTTOM_LEFT",
+        "BOTTOM_MIDDLE",
+        "BOTTOM_RIGHT",
+        "NULL",
+      ],
+    },
+  },
+});
+
+const move = type(
+  "'TOP_LEFT' | 'TOP_MIDDLE' | 'TOP_RIGHT' | 'MIDDLE_LEFT' | 'MIDDLE_MIDDLE' | 'MIDDLE_RIGHT' | 'BOTTOM_LEFT' | 'BOTTOM_MIDDLE' | 'BOTTOM_RIGHT'",
+);
+
+type Move = typeof move.infer;
+
+const keys: Record<Move, number> = {
+  TOP_LEFT: 0,
+  TOP_MIDDLE: 1,
+  TOP_RIGHT: 2,
+  MIDDLE_LEFT: 3,
+  MIDDLE_MIDDLE: 4,
+  MIDDLE_RIGHT: 5,
+  BOTTOM_LEFT: 6,
+  BOTTOM_MIDDLE: 7,
+  BOTTOM_RIGHT: 8,
+};
+
+async function parseMove(transcript: string) {
+  const result = await model.generateContent(
+    `Parse out the tic tac toe move the following text implies return a null if you can't find a move: ${transcript}`,
+  );
+  const text = result.response.text();
+  const m = move(text);
+
+  if (m instanceof ArkErrors) {
+    return null;
+  }
+
+  return keys[m];
+}
